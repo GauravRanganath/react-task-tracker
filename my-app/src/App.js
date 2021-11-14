76cc71c9046945c3a2cb3c763672ecf0431c2576
Gaurav Ranganath <gaurav.ranganath@mail.utoronto.ca>
2021-11-14 18:04:47 -0500

    Add files

---
+++ my-app/src/App.js
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import FormControl from './components/AddTask'
+import AddTask from './components/AddTask'
+
+function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder:  true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder:  true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder:  false,
+    },
+  ])
+
+  const name = "Gaurav"
+  const x = false
+
+  const addTask = (task) => {
+    console.log(task)
+  }
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
+  return (
+    <div className="container">
+      <Header title="Task Tracker"/>
+      <AddTask onAdd={addTask}/>
+      {tasks.length > 0 ? 
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+        : 'No Tasks To Show'
+      }
+    </div>
+  );
+}
+
+export default App;
